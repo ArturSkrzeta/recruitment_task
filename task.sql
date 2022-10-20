49c7eaf59630d66a63ba20453bdff985bb4dbae5
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2022-10-20 15:29:37 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 SELECT a.dimension_1, map.correct_dimension_2, SUM(a.measure_1), SUM(b.measure_2)
 FROM TABLE_A AS a
-FULL OUTER JOIN TABLE_B as b
-LEFT JOIN TABLE_MAP AS map on a.dimension_1 = map.dimension_1
+FULL OUTER JOIN TABLE_B as b ON a.dimension_1 = b.dimension_1
+LEFT JOIN TABLE_MAP AS map ON a.dimension_1 = map.dimension_1
 GROUP BY 1, 2
